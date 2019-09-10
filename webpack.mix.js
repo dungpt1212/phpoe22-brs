fa466621b29160c3d2c5df14c7bb1164aaa2cd66
dungpt <phamtrungdung1212@gmail.com>
2019-09-10 23:19:01 +0700

    searchBook

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,10 +24,13 @@ mix.js('resources/js/app.js', 'public/js')
         'public/assets/lib/js/active.js',
         'resources/js/book_detail.js',
         'resources/js/follow.js',
+        'resources/js/dataTable.js',
         'public/assets/lib/lib-admin/js/main.js',
         'public/assets/lib/lib-admin/js/plugins/pace.min.js',
         'public/assets/lib/lib-admin/js/plugins/chart.js',
         'public/assets/lib/lib-admin/js/something.js',
+
+
 
     ], 'public/js/custom.js')
     .sass('resources/sass/app.scss', 'public/css');
